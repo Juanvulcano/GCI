b348f6a5ea792348a8fc0e520c36036ef4b0fd52
Juan F. Verhook <jfverhook@hotmail.com>
2015-12-30 02:47:50 -0430

    Added first script written with Light Table

---
+++ Fossasia/Week3/light.py
@@ -0,0 +1,11 @@
+def GCI2015():
+  print("Do you code?")
+  a = input("Please answer yes/no ")
+  if a == "yes":
+    print("May the source be with you")
+  elif a == "no":
+    print("You are not worthy of the terminal!")
+  else:
+    print("Not a valid input, script just accept 'yes' or 'no'")
+
+GCI2015()
